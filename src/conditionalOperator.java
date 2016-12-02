31ce8970c44ebd4b386fa246d470860703f97aae
Resham Khadka <bisshh@users.noreply.github.com>
2016-12-02 13:46:51 +0545

    Delete conditionalOperator.java

--- src/conditionalOperator.java
+++
@@ -1,8 +0,0 @@
-
-public class conditionalOperator {
-	public static void main(String args[]) {
-		int a=4;
-		String result = (a%2 != 0) ? "The number is odd." : "The number is even.";
-		System.out.println(result);
-	}
-}
