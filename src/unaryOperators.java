edda8fca830cb1c3952187f8bd9f9d3d9faa8ae6
Resham Khadka <bisshh@users.noreply.github.com>
2016-12-02 13:46:03 +0545

    Delete unaryOperators.java

--- src/unaryOperators.java
+++
@@ -1,9 +0,0 @@
-public class unaryOperators {
-	public static void main(String args[]){
-		int a = 15;
-		System.out.println(a);
-		System.out.println(a++);
-		System.out.println(a--);
-		System.out.println(a);		
-	}
-}
