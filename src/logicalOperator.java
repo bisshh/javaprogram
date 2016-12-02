f350a07fab41ad2124fcda05754101d3c52fb7e2
Resham Khadka <bisshh@users.noreply.github.com>
2016-12-02 13:46:29 +0545

    Delete logicalOperator.java

--- src/logicalOperator.java
+++
@@ -1,9 +0,0 @@
-
-public class logicalOperator {
-	public static void main (String args[]){
-		int a=2, b=4, c=6 ;
-		System.out.println(a>b && a<c);
-		System.out.println(a!=b || a<b);
-		System.out.println(!(a<b));
-	}
-}
