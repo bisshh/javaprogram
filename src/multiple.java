af12eb3cfcff2ba11669e1cdfd00c00ee31649c3
Resham Khadka <bisshh@users.noreply.github.com>
2016-12-02 13:46:20 +0545

    Delete multiple.java

--- src/multiple.java
+++
@@ -1,8 +0,0 @@
-
-public class multiple {
-	public static void main (String args[]){
-		int a=32, b=52, c=58, d=48, e=85;
-		String result = (a >= 32 && a <= 100 && b >= 32 && b <= 100 && c >= 32 && c <= 100 && d >= 32 && d <= 100 && e >= 32 && e <= 100) ? "Student is pass." : "Student is fail.";
-		System.out.println(result);
-	}
-}
